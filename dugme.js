2392b23c41a71b153a0fc2b458c6e44e026ef4c8
ivanhoeNS <32396088+ivanhoeNS@users.noreply.github.com>
2018-12-11 14:37:30 +0100

    Add files via upload

---
+++ dugme.js
@@ -0,0 +1,9 @@
+
+var button= document.getElementById('textButton');
+
+button.addEventListener ("click",redirekcija);
+
+function redirekcija() {
+
+	window.location = "https://www.tripadvisor.rs";
+}
